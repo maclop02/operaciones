9fe800cc3532344303cf430d38b2dabc9cc9c8f0
maclop02 <maclop00@yahoo.com>
2024-07-15 17:52:22 -0500

    Add files via upload

---
+++ areas.py
@@ -0,0 +1,13 @@
+import math
+
+def area_circulo(radio):
+    """Calcula el área de un círculo dado su radio."""
+    return math.pi * radio ** 2
+
+def area_rectangulo(base, altura):
+    """Calcula el área de un rectángulo dada su base y altura."""
+    return base * altura
+
+def area_triangulo(base, altura):
+    """Calcula el área de un triángulo dada su base y altura."""
+    return (base * altura) / 2
---
+++ paquete_areas.py
@@ -0,0 +1,5 @@
+
+import areas
+print(areas.area_circulo(5))
+print(areas.area_rectangulo(4, 5))
+print(areas.area_triangulo(4, 5))
